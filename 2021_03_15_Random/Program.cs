c94e4bf2715522e465badedbf1ed698286099b69
Linas <66268230+linas92@users.noreply.github.com>
2021-03-17 19:26:41 +0200

    Add project files.

---
+++ 2021_03_15_Random/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2021_03_15_Random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random Rng = new Random();
+
+            int aRandomNumber = Rng.Next();
+            int aRandomNumber1 = Rng.Next(4);
+            int aRandomNumber2 = Rng.Next(1,4);
+
+            var bRandomSkaicius = Rng.NextDouble();
+
+            Metodas(Rng);
+
+            Console.WriteLine("END OF THE LINE BUDDY");
+            Console.ReadKey();
+        }
+        public static int Metodas(Random rnd)
+        {
+            return rnd.Next(1,10);
+        }
+    }
+}
---
+++ 2021_03_15_Random/RNGCryptoServiceProvider.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2021_03_15_Random
+{
+    internal class RNGCryptoServiceProvider
+    {
+        public RNGCryptoServiceProvider()
+        {
+        }
+
+        public static implicit operator System.Security.Cryptography.RNGCryptoServiceProvider(RNGCryptoServiceProvider v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
